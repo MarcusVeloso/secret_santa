55b751578e2d681d5b2e25c508a11ac079834a50
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-04-18 23:15:39 -0300

    add models and helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,48 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/destroy'
+
+  get 'members/update'
+
+  get 'campaign/show'
+
+  get 'campaign/index'
+
+  get 'campaign/create'
+
+  get 'campaign/update'
+
+  get 'campaign/destroy'
+
+  get 'campaign/raffle'
+
+  get 'pages/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/destroy'
+
+  get 'members/update'
+
+  get 'campaign/show'
+
+  get 'campaign/index'
+
+  get 'campaign/create'
+
+  get 'campaign/update'
+
+  get 'campaign/destroy'
+
+  get 'campaign/raffle'
+
+  get 'pages/home'
+
     devise_for :users, :controllers => { registrations: 'registrations' }
      mount Sidekiq::Web => '/sidekiq'
   end
