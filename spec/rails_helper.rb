f48f60960e4fc0bb76c3060a8802d25b0b7f8659
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-04-20 17:35:43 -0300

    udpate rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,13 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,5 +68,3 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
