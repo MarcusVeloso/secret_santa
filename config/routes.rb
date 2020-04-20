e2475619f7fde6edc66bf4a09a8d4cb012530b99
Marcus Veloso <desenvolvedoroficial@gmail.com>
2020-04-20 16:04:17 -0300

    update routes info

--- config/routes.rb
+++ config/routes.rb
@@ -1,49 +1,14 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'members/create'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  mount Sidekiq::Web => '/sidekiq'
 
-  get 'members/destroy'
-
-  get 'members/update'
-
-  get 'campaign/show'
-
-  get 'campaign/index'
-
-  get 'campaign/create'
-
-  get 'campaign/update'
-
-  get 'campaign/destroy'
-
-  get 'campaign/raffle'
-
-  get 'pages/home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-  get 'members/create'
-
-  get 'members/destroy'
-
-  get 'members/update'
-
-  get 'campaign/show'
-
-  get 'campaign/index'
-
-  get 'campaign/create'
-
-  get 'campaign/update'
-
-  get 'campaign/destroy'
-
-  get 'campaign/raffle'
-
-  get 'pages/home'
-
-    devise_for :users, :controllers => { registrations: 'registrations' }
-     mount Sidekiq::Web => '/sidekiq'
+  root to: 'pages#home'
+  resources :campaigns, except: [:new] do
+    post 'raffle', on: :member
+    # post 'raffle', on: :collection
   end
+  get 'members/:token/opened', to: 'members#opened'
+  resources :members, only: [:create, :destroy, :update]
 end
